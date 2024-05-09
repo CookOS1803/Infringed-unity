71dd95a39aa3ceaca80ef81f7015dbf000f4e3a4
CookOS <usovich5321@gmail.com>
2024-05-09 15:27:17 +0300

    editor and probuilder

--- Assets/Code/Map/Editor/FloorEditor.cs
+++ Assets/Code/Map/Editor/FloorEditor.cs
@@ -2,15 +2,103 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Infringed.Math;
 
 namespace Infringed.Map.Editor
 {
     [CustomEditor(typeof(Floor))]
     public class FloorEditor : UnityEditor.Editor
     {
-        private Floor _floor => (Floor)target;
+        private Floor _floor => target as Floor;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Update Walls"))
+            {
+                _floor.UpdateWalls();
+                EditorUtility.SetDirty(_floor);
+            }
+        }
 
         private void OnSceneGUI()
+        {
+            foreach (var spline in _floor.Splines)
+            {
+                var nodeCount = spline.Nodes.Count;
+
+                if (nodeCount == 0)
+                    continue;
+
+                DrawSplineNodes(spline);
+
+                if (spline.Cap && nodeCount > 2)
+                {
+                    var from = _floor.transform.TransformPoint(spline.Nodes[nodeCount - 1]);
+                    var to = _floor.transform.TransformPoint(spline.Nodes[0]);
+
+                    Handles.DrawLine(from, to);
+                }
+
+                int interpolationCount;
+
+                if (spline.InterpolatedPoints == null || (interpolationCount = spline.InterpolatedPoints.Length) < 2)
+                    continue;
+
+                Handles.color = Color.white;
+
+                for (int i = 1; i < interpolationCount; i++)
+                {
+                    var from = _floor.transform.TransformPoint(spline.InterpolatedPoints[i - 1]);
+                    var to = _floor.transform.TransformPoint(spline.InterpolatedPoints[i]);
+
+                    Handles.DrawLine(from, to);
+                }
+
+                if (spline.Cap)
+                {
+                    var from = _floor.transform.TransformPoint(spline.InterpolatedPoints[interpolationCount - 1]);
+                    var to = _floor.transform.TransformPoint(spline.InterpolatedPoints[0]);
+
+                    Handles.DrawLine(from, to);
+                }
+            }
+
+            _floor.UpdateSplineInterpolations();
+        }
+
+        private void DrawSplineNodes(Spline spline)
+        {
+            Handles.color = Color.red;
+
+            DrawPositionHandle(spline, 0);
+
+            for (int i = 1; i < spline.Nodes.Count; i++)
+            {
+                var from = _floor.transform.TransformPoint(spline.Nodes[i - 1]);
+                var to = _floor.transform.TransformPoint(spline.Nodes[i]);
+
+                Handles.DrawLine(from, to);
+
+                DrawPositionHandle(spline, i);
+            }
+        }
+
+        private void DrawPositionHandle(Spline spline, int index)
+        {
+            var globalNode0 = _floor.transform.TransformPoint(spline.Nodes[index]);
+            var newGlobalNode0 = Handles.PositionHandle(globalNode0, Quaternion.identity);
+
+            if (newGlobalNode0 != globalNode0)
+            {
+                EditorUtility.SetDirty(_floor);
+            }
+
+            spline.Nodes[index] = _floor.transform.InverseTransformPoint(newGlobalNode0);
+        }
+
+        private void testkal()
         {
             var cameraRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
             var floorWasHit = Physics.Raycast(cameraRay, out var floorHit, Mathf.Infinity);
@@ -35,7 +123,6 @@ namespace Infringed.Map.Editor
 
                 menu.ShowAsContext();
             }
-
         }
     }
 }
--- Assets/Code/Map/Floor.cs
+++ Assets/Code/Map/Floor.cs
@@ -1,11 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using Infringed.Math;
 using UnityEngine;
 
 namespace Infringed.Map
 {
     public class Floor : MonoBehaviour
     {
-        
+#if UNITY_EDITOR
+        [SerializeField] private GameObject _wallsPrefab;
+        [SerializeField] private Transform _wallsParent;
+        [field: SerializeField] public List<Spline> Splines { get; private set; } = new();
+
+        public void UpdateSplineInterpolations()
+        {
+            foreach (var spline in Splines)
+            {
+                spline.UpdateInterpolation();
+            }
+        }
+
+        public void UpdateWalls()
+        {
+            var difference = _wallsParent.childCount - Splines.Count;
+
+            if (difference > 0)
+            {
+                while (difference > 0)
+                {
+                    DestroyImmediate(_wallsParent.GetChild(0).gameObject);
+                    difference--;
+                }
+            }
+            else if (difference < 0)
+            {
+                while (difference < 0)
+                {
+                    Instantiate(_wallsPrefab, _wallsParent);
+                    difference++;
+                }
+            }
+
+            foreach (var spline in Splines)
+            {
+                
+            }
+        }
+#endif
+
+
     }
 }
